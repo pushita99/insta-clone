6ba1d780b8527a0dc725b22420c0f4d480985a2e
pushita99 <pushita990301@gmail.com>
2024-08-19 18:50:19 +0200

    custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import AuthPage from "./pages/AuthPage/AuthPage.jsx";
+import PageLayout from "./components/Layouts/PageLayout/PageLayout.jsx";
 
 function App() {
 
   return (
-    <>
+    <PageLayout>
      <Routes>
          <Route path='/' element={<HomePage/>}/>
          <Route path='/auth' element={<AuthPage/>}/>
      </Routes>
-    </>
+    </PageLayout>
   )
 }
 
